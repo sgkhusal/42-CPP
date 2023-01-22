382f92c3fec0df2b2a88d12048cd142519037188
sgkhusal <sheela.khusal.08@gmail.com>
2023-01-22 12:38:48 -0300

    Add prints with colors

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: sguilher <sguilher@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 01:04:16 by sguilher          #+#    #+#             */
-/*   Updated: 2023/01/17 01:32:38 by sguilher         ###   ########.fr       */
+/*   Updated: 2023/01/20 16:22:15 by sguilher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,34 @@
 
 const int Fixed::_fractional_bits = 8;
 
-Fixed::Fixed(void): _nb(0) {
-	std::cout << "Default constructor called" << std::endl;
+Fixed::Fixed(void): _rawBits(0) {
+	std::cout << GREEN << "Default constructor called" << RESET << std::endl;
 	return ;
 }
 
-Fixed::Fixed(Fixed const& src) {
-	std::cout << "Copy constructor called" << std::endl;
-	*this = src;
+Fixed::Fixed(Fixed const& fixed) {
+	std::cout << GREEN << "Copy constructor called" << RESET << std::endl;
+	*this = fixed;
 	return ;
 }
 
 Fixed::~Fixed(void) {
-	std::cout << "Destructor called" << std::endl;
+	std::cout << ORANGE << "Destructor called" << RESET << std::endl;
 }
 
-Fixed& Fixed::operator=(Fixed const& src) {
-	std::cout << "Copy assignment operator called" << std::endl;
-	if (this != &src)
-		this->_nb = src.getRawBits();
+Fixed& Fixed::operator=(Fixed const& fixed) {
+	std::cout << BLUE << "Copy assignment operator called" << RESET << std::endl;
+	if (this != &fixed)
+		this->_rawBits = fixed.getRawBits();
 	return (*this);
 }
 
 int	Fixed::getRawBits(void) const {
-	std::cout << "getRawBits member function called" << std::endl;
-	return (this->_nb);
+	std::cout << GREY << "getRawBits member function called" << RESET << std::endl;
+	return (this->_rawBits);
 }
 
 void	Fixed::setRawBits(int const raw) {
-	this->_nb = raw;
+	this->_rawBits = raw;
 	return ;
 }
--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: sguilher <sguilher@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 01:04:19 by sguilher          #+#    #+#             */
-/*   Updated: 2023/01/17 01:25:14 by sguilher         ###   ########.fr       */
+/*   Updated: 2023/01/20 16:22:15 by sguilher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,23 @@
 
 #include <iostream>
 
+# define ORANGE	"\033[0;38;5;166m"
+# define BLUE	"\033[38;5;75m"
+# define GREEN	"\033[1;32m"
+# define GREY	"\033[38;5;244m"
+# define RESET	"\033[0m"
+
 class Fixed {
 	public:
 		Fixed(void);
-		Fixed(Fixed const& src);
+		Fixed(Fixed const& fixed);
 		~Fixed(void);
-		Fixed& operator=(Fixed const& src);
+		Fixed& operator=(Fixed const& fixed);
 
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
 	private:
-		int _nb;
+		int _rawBits;
 		static const int _fractional_bits;
 };
 
